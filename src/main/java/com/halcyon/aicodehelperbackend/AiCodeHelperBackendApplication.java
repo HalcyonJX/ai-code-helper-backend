d650eac45a26b6cdad27d67d629596f5ca87ae73
HalcyonJX <502229027@qq.com>
2025-11-12 21:59:08 +0800

    AI编程助手：
    LangChain4j特性：
    
    1、简单对话
    
    2、多模态对话

---
+++ src/main/java/com/halcyon/aicodehelperbackend/AiCodeHelperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.halcyon.aicodehelperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeHelperBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeHelperBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halcyon/aicodehelperbackend/AiCodeHelperBackendApplicationTests.java
@@ -0,0 +1,30 @@
+package com.halcyon.aicodehelperbackend;
+
+import com.halcyon.aicodehelperbackend.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperBackendApplicationTests {
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    @Test
+    void chat() {
+        aiCodeHelper.chat("你好，50个字以内介绍Faker");
+    }
+
+    @Test
+    void chatWithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://www.codefather.cn/logo.png")
+        );
+        aiCodeHelper.chatWithMessage(userMessage);
+    }
+
+}
